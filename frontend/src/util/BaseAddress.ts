3a1af5cdf23a8f2a96fe8896d50ae6a14f13bb66
kaiadachi <kaikaikai8217@gmail.com>
2023-12-10 02:23:32 +0530

    update: bytecode

--- frontend/src/util/BaseAddress.ts
+++ frontend/src/util/BaseAddress.ts
@@ -1,4 +1,4 @@
-export const erc7546Proxy = "0xDf29781C047425665f47b0417E1eCEBE9FCb1a95";
+export const erc7546Proxy = "0x91bB238EBFd16611047534a4f3816f7Ed8f9f83d";
 export const lightAccountImpl = "0xf80c1D36a5A9621f883940A13fCFCe1d6336580A";
 export const entryPoint = "0xB31499445F1cC556a66cdeeF2fd56766D03abA00";
 export const dictionary = "0xBf0722aAf257071F128653B6552F149205d3294d";
