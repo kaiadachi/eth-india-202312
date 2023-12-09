e42a636541e7e78ce7ff6489c5cf791121d3ada2
kaiadachi <kaikaikai8217@gmail.com>
2023-12-10 05:07:29 +0530

    add: pdf

--- frontend/src/util/BaseAddress.ts
+++ frontend/src/util/BaseAddress.ts
@@ -1,6 +1,6 @@
-export const erc7546Proxy = "0x91bB238EBFd16611047534a4f3816f7Ed8f9f83d";
+export const erc7546Proxy = "0x155104a4bbfdc87AD7cb2f8fFC5B6d1a4D416F07";
 export const lightAccountImpl = "0xf80c1D36a5A9621f883940A13fCFCe1d6336580A";
 export const entryPoint = "0xB31499445F1cC556a66cdeeF2fd56766D03abA00";
 export const dictionary = "0xBf0722aAf257071F128653B6552F149205d3294d";
 export const safeImpl = "0x40dbB5ACcCbEf77B4d59dC70478133631b19231d";
-export const receiver = "0x5b01BeEa584956568445fbA3dEA0a7B72e6DeC6A";
+export const receiver = "0xd854F8fBd1d7dB3259984E53720fd51237248358";
